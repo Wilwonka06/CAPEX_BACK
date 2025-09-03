6b580282a0d6382be52dcf59ccf3594f8e4bcb06
Wilwonka06 <heieihei183@gmail.com>
2025-09-02 19:59:44 -0500

    prueba vercel #2

--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
-require('dotenv').config();
-/* const app = require('./app'); */
-/*  */
-/* const PORT = process.env.PORT || 3000; */
-/*  */
-/* app.listen(PORT, () => { */
-/*   console.log(`Servidor corriendo en el puerto ${PORT}`); */
-/* }); */
-/*  */
-/* const app = require('./app'); */
-/* module.exports = app; // 👈 en Vercel solo exportamos */
-/*  */
-/*  */
+/* require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+ */
